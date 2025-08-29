1f9601b3b88dd0893d23aee806df1f946867b33d
Augusto Coronel <aoc@getgoogleoff.me>
2025-08-29 16:44:11 -0300

    docs: Documentate bpaths

--- src/neostow/bpaths.nim
+++ src/neostow/bpaths.nim
@@ -1,6 +1,15 @@
+##[
+Helper module to handle paths.
+]##
+
 import std/strutils
 
 proc headDir*(file: string): string =
+  ##[
+  Returns the very first directory from a string. If no directory is recognized, returns the string as is.
+
+  It serves a specific use case in neostow, to collect an environment variable in the destination field and futurely expand it.
+  ]##
   for i in file:
     if i == '/':
       break
@@ -8,16 +17,26 @@ proc headDir*(file: string): string =
   return result
 
 proc getchar*(s: string, c: char): string =
-  for i in 0..<s.len:
+  ##[
+  Consumes a given character from a given string, only at the first occurence
+
+  It serves to consume the `$` symbol from environment variables.
+  ]##
+  for i in 0 ..< s.len:
     if s[i] == c:
-      return s[0..<i] & s[i+1..^1]
+      return s[0 ..< i] & s[i + 1 ..^ 1]
   return s
 
 proc basename*(path: string): string =
+  ##[
+  Returns the name of directory or filename, without the `/` separator.
+  ]##
   var skipSep = 1
   if path.endsWith('/'):
     skipSep = 2
   for i in countdown(path.len - skipSep, 0):
     if path[i] == '/':
-      return path[i+1 .. ^skipSep]
+      return path[i + 1 .. ^skipSep]
   return path[0 .. ^skipSep]
+
+echo headDir("/home/anstrinn/stow/secrets")
